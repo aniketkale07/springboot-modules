367dbc809308aaeb84fb77879c92102124cf77ed
Aniket Kale <164205726+aniketkale07@users.noreply.github.com>
2025-04-03 10:49:18 +0530

    added getResponseOptions

--- open-ai-module/src/main/java/com/ai/service/OpenAIService.java
+++ open-ai-module/src/main/java/com/ai/service/OpenAIService.java
@@ -1,6 +1,9 @@
 package com.ai.service;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +18,18 @@ public class OpenAIService {
     // service method who can easily to get back response to the controller
     public String getResponse(String prompt){
         return chatModel.call(prompt);
-        
+    }
+
+    
+    public String getResponseOptions(String prompt){
+        ChatResponse chatResponse= chatModel.call(
+            new Prompt(prompt, 
+            OpenAiChatOptions.builder()
+                .model("gpt-4o")    // .withModel --> changed to .model
+                .temperature(4.0d)  // .withTemperature to temperature --> cast long to double
+                .build()
+            )
+        );
+        return chatResponse.getResult().getOutput().getText();
     }
 }
